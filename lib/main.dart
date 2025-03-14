ae0f4f3d1b257ac531559cfa7a8f0f525abb6757
@tools2025 <xdtools9@gmail.com>
2025-03-14 20:40:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
-import 'dart:io'; // Tambahkan ini untuk Platform, Directory, dan File
 import 'dart:convert';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart'; // Import services.dart
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter_file_dialog/flutter_file_dialog.dart';
+import 'package:url_launcher/url_launcher.dart'; 
+import 'file_downloader.dart'; 
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Set warna status bar dan bar navigasi
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
-      statusBarColor: Color(0xFF8B5CF6),
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: Colors.white,
+      statusBarColor: Color(0xFF8B5CF6), 
+      statusBarIconBrightness: Brightness.light, 
+      systemNavigationBarColor: Colors.white, 
       systemNavigationBarIconBrightness: Brightness.dark,
     ),
   );
@@ -40,8 +37,14 @@ class WebViewScreen extends StatefulWidget {
   _WebViewScreenState createState() => _WebViewScreenState();
 }
 
+class WebViewScreen extends StatefulWidget {
+  @override
+  _WebViewScreenState createState() => _WebViewScreenState();
+}
+
 class _WebViewScreenState extends State<WebViewScreen> {
-  late final WebViewController _controller;
+  late WebViewController _controller;
+  final FileDownloader _fileDownloader = FileDownloader(); // Buat instance FileDownloader
 
   @override
   void initState() {
@@ -51,6 +54,8 @@ class _WebViewScreenState extends State<WebViewScreen> {
       ..setUserAgent("enzoXzodix")
       ..setNavigationDelegate(
         NavigationDelegate(
+          onPageStarted: (_) {},
+          onPageFinished: (_) {},
           onWebResourceError: (error) async {
             String htmlString = await rootBundle.loadString('assets/404.html');
             _controller.loadRequest(
@@ -71,70 +76,24 @@ class _WebViewScreenState extends State<WebViewScreen> {
           },
         ),
       )
+      ..addJavaScriptChannel('Flutter', onMessageReceived: (message) {
+        if (message.message == 'downloadFile') {
+          _fileDownloader.downloadFile('URL_FILE', 'nama_file.txt'); // Panggil metode downloadFile
+        }
+      })
       ..loadRequest(Uri.parse('https://coder8-33-63.vercel.app/'));
   }
 
   Future<void> _launchExternalBrowser(String url) async {
-    if (await canLaunchUrl(Uri.parse(url))) {
-      await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
-    } else {
-      print('Could not launch $url');
-    }
-  }
-
-  void _promptUserForFileName(BuildContext context, Uint8List data, String mimeType) {
-    TextEditingController controller = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Save File"),
-          content: TextField(
-            controller: controller,
-            decoration: InputDecoration(hintText: "Enter filename"),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text("Cancel"),
-            ),
-            TextButton(
-              onPressed: () async {
-                String fileName = controller.text.trim().isNotEmpty
-                    ? controller.text.trim()
-                    : "XD-TOOLS_${DateTime.now().millisecondsSinceEpoch}.txt";
-
-                await _saveFileToDownloads(fileName, data, mimeType);
-                Navigator.pop(context);
-              },
-              child: Text("Save"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _saveFileToDownloads(String fileName, Uint8List data, String mimeType) async {
-    if (Platform.isAndroid) {
-      var status = await Permission.storage.request();
-      if (!status.isGranted) {
-        return;
+    try {
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
       }
+    } catch (e) {
+      print('Error launching URL: $e');
     }
-
-    Directory? downloadsDir = await getExternalStorageDirectory();
-    if (downloadsDir == null) return;
-
-    String downloadsPath = "${downloadsDir.path}/DECODE";
-    downloadsDir.createSync(recursive: true); // Perbaiki cara membuat folder
-
-    File file = File("$downloadsPath/$fileName");
-    await file.writeAsBytes(data);
-
-    final params = SaveFileDialogParams(sourceFilePath: file.path);
-    await FlutterFileDialog.saveFile(params: params);
   }
 
   @override
@@ -145,23 +104,7 @@ class _WebViewScreenState extends State<WebViewScreen> {
         elevation: 0,
         backgroundColor: Color(0xFF8B5CF6),
       ),
-      body: Stack(
-        children: [
-          WebViewWidget(controller: _controller),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: FloatingActionButton(
-              onPressed: () async {
-                Uint8List data = Uint8List.fromList(utf8.encode("Sample text file"));
-                _promptUserForFileName(context, data, "text/plain");
-              },
-              child: Icon(Icons.save),
-              backgroundColor: Color(0xFF8B5CF6),
-            ),
-          ),
-        ],
-      ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
