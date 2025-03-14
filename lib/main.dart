f5680b5ba5171444887fdd9fc81c1ee09f018284
@tools2025 <xdtools9@gmail.com>
2025-03-14 22:38:04 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart'; // Import services.dart
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:file_picker/file_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart'; 
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Set status bar and navigation bar colors
+  // Set warna status bar dan bar navigasi
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
-      statusBarColor: Color(0xFF8B5CF6),
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: Colors.white,
+      statusBarColor: Color(0xFF8B5CF6), 
+      statusBarIconBrightness: Brightness.light, 
+      systemNavigationBarColor: Colors.white, 
       systemNavigationBarIconBrightness: Brightness.dark,
     ),
   );
@@ -51,7 +47,9 @@ class _WebViewScreenState extends State<WebViewScreen> {
       ..setUserAgent("enzoXzodix")
       ..setNavigationDelegate(
         NavigationDelegate(
-          onPageStarted: (_) {},
+          onPageStarted: (_) {
+            // Anda bisa menambahkan logika di sini jika diperlukan
+          },
           onPageFinished: (_) {},
           onWebResourceError: (error) async {
             String htmlString = await rootBundle.loadString('assets/404.html');
@@ -64,9 +62,11 @@ class _WebViewScreenState extends State<WebViewScreen> {
             );
           },
           onNavigationRequest: (request) {
+            // Handle navigasi ke URL lain
             if (request.url.startsWith('https://coder8-33-63.vercel.app/')) {
               return NavigationDecision.navigate;
             } else {
+              // Buka URL di browser eksternal
               _launchExternalBrowser(request.url);
               return NavigationDecision.prevent;
             }
@@ -88,86 +88,15 @@ class _WebViewScreenState extends State<WebViewScreen> {
     }
   }
 
-  // Request storage permission
-  Future<bool> _requestStoragePermission() async {
-    final permissionStatus = await Permission.manageExternalStorage.request();
-
-    if (permissionStatus.isGranted) {
-      return true;
-    } else {
-      print("Storage permission denied.");
-      return false;
-    }
-  }
-
-  // Pick a file using the file picker
-  Future<void> _pickFile() async {
-    // Request permission before picking a file
-    bool hasPermission = await _requestStoragePermission();
-    if (!hasPermission) {
-      return;
-    }
-
-    // Pick a file using the file picker
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
-
-    if (result != null) {
-      File file = File(result.files.single.path!);
-      print('Picked file: ${file.path}');
-      // Handle the file, e.g., read, save, etc.
-    } else {
-      print('No file picked');
-    }
-  }
-
-  // Save the content to a file in Downloads/Decoder directory
-  Future<void> _saveFile(String fileName, String content) async {
-    bool hasPermission = await _requestStoragePermission();
-    if (!hasPermission) {
-      return;
-    }
-
-    try {
-      final directory = await _getDownloadsDirectory();
-      if (directory != null) {
-        final file = File('${directory.path}/Decoder/$fileName');
-        await file.create(recursive: true);
-        await file.writeAsString(content);
-        print('File saved at: ${file.path}');
-      }
-    } catch (e) {
-      print('Error saving file: $e');
-    }
-  }
-
-  // Get the path to the Downloads directory
-  Future<Directory?> _getDownloadsDirectory() async {
-    final directory = await getExternalStorageDirectory();
-    if (directory != null) {
-      final downloadsDir = Directory('${directory.path}/Downloads');
-      if (!await downloadsDir.exists()) {
-        await downloadsDir.create(recursive: true);
-      }
-      return downloadsDir;
-    }
-    return null;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 0,
+        toolbarHeight: 0, 
         elevation: 0,
-        backgroundColor: Color(0xFF8B5CF6),
+        backgroundColor: Color(0xFF8B5CF6), 
       ),
       body: WebViewWidget(controller: _controller),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _pickFile, // Trigger file picker
-        child: ImageIcon(AssetImage('assets/ic_launcher.png')),
-
-      ),
     );
   }
 }
- 
